e49cb07b5f4e10a6d59538efabc38e5a5487f636
ramriddlez <ramankangz@outlook.com>
2022-06-25 17:48:33 -0400

    added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-let firstName = document.getElementById("fname").innerText
-let lastName = document.getElementById("lname").innerText
+let firstName = document.getElementById("fname").value;
+let lastName = document.getElementById("lname").value;
 
 console.log(firstName)
 
 function alertMessage() {
-    alert("Hi my name is" + firstName + lastName )
+    alert(`Hi my name is + ${firstName} + ${lastName}`)
 }
