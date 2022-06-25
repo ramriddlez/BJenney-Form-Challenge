f33ecfbfc3e87c13c9e7423bff41a1c0b89d4734
ramriddlez <ramankangz@outlook.com>
2022-06-25 18:06:26 -0400

    added onkey function and if statement with disability toggle

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ console.log(firstName)
 function alertMessage() {
     alert(`Hi my name is + ${firstName} + ${lastName}`)
 }
+
+function isEmpty() {
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+
+    if (firstName != '' && lastName != '') {
+        document.getElementById("submit").removeAttribute("disabled")
+    }
+}
