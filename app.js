af964dccd09acfbcceea832c5700b1db4626b1f1
ramriddlez <ramankangz@outlook.com>
2022-06-25 17:45:15 -0400

    added alert

---
+++ app.js
@@ -0,0 +1,8 @@
+let firstName = document.getElementById("fname").innerText
+let lastName = document.getElementById("lname").innerText
+
+console.log(firstName)
+
+function alertMessage() {
+    alert("Hi my name is" + firstName + lastName )
+}
